afcb30f8794f64c592f8c980e2ad5158667c1f64
Sunny Carlin Qi <sq2284@columbia.edu>
2025-02-15 17:05:39 -0500

    Client and server successfully read from each other

--- client.py
+++ client.py
@@ -7,11 +7,14 @@
 
 import threading
 from queue import Queue
-from video_player import play_chunks
+# from video_player import play_chunks #only needed for video laying
 import sys
 
-def client(server_addr, server_port, video_name, alpha, chunks_queue):
-    """
+# added myself
+import socket
+import os
+
+"""
     the client function
     write your code here
 
@@ -21,12 +24,46 @@ def client(server_addr, server_port, video_name, alpha, chunks_queue):
     video_name -- the name of the video
     alpha -- the alpha value for exponentially-weighted moving average
     chunks_queue -- the queue for passing the path of the chunks to the video player
+
+"""
+
+def client(server_addr, server_port, video_name, alpha, chunks_queue):
+    # setup socket and files
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((server_addr, server_port))
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+
+    # send request for manifest file of video and recieve
+    clientSocket.send(video_name.encode())
+    result = clientSocket.recv(64).decode()
+    print(result)
+    if result == "error: not found" or result == "error: IO failed": #failed
+        clientSocket.close() #DC from server if not found
+        return -1
+        
+    
+    #mpd_file = clientSocket.recv(4096)
+
     """
+    chunk_num = 0
+    while (True): #keep on collecting data till there's none left
+        chunk = clientSocket.recv(1024)
+        if not chunk: #finished receiving data
+            break
+        with open(f"tmp/chunk_{chunk_num}.m4s", "wb") as f:
+            f.write(chunk)
+        chunks_queue.put(f"tmp/chunk_{chunk_num}.m4s")
+        chunk_num += 1
+    """
+
+    clientSocket.close()
+    return
 
     # to visualize the adaptive video streaming, store the chunk in a temporary folder and
     # pass the path of the chunk to the video player
     # 
-    # # create temporary directory if not exist
+    # create temporary directory if not exist
     # if not os.path.exists("tmp"):
     #     os.makedirs("tmp")
     # # write chunk to the temporary directory
@@ -34,8 +71,6 @@ def client(server_addr, server_port, video_name, alpha, chunks_queue):
     #     f.write(chunk)
     # # put the path of the chunk to the queue
     # chunks_queue.put(f"tmp/chunk_0.m4s")
-
-    pass
 
 
 # parse input arguments and pass to the client function
@@ -51,4 +86,4 @@ if __name__ == '__main__':
     client_thread = threading.Thread(target = client, args =(server_addr, server_port, video_name, alpha, chunks_queue))
     client_thread.start()
     # start the video player
-    play_chunks(chunks_queue)
+    # play_chunks(chunks_queue) //only needed for video playing
--- server.py
+++ server.py
@@ -5,3 +5,43 @@
 # server.py - the server program for taking request from the client and 
 #             send the requested file back to the client
 #
+import socket
+serverPort = 50000
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
+print("Server is ready to recieve connections")
+
+def open_file(request, connectionSocket):
+    try:
+        file = open(request, "rb")
+        success_msg = "success: file exist"
+        connectionSocket.send(success_msg.encode())
+        return file
+    except FileNotFoundError:
+        err_msg = "error: not found"
+        connectionSocket.send(err_msg.encode())
+        connectionSocket.close() # DC from client if fail
+        return -1
+    except IOError:
+        err_msg = "error: IO failed"
+        connectionSocket.send(err_msg.encode())
+        connectionSocket.close()
+        return -1
+
+while True:
+    # recieve manifest file request
+    connectionSocket, clientAddr = serverSocket.accept()
+    video_name = connectionSocket.recv(1024).decode()
+    mpd_request = "./data/" + video_name + "/manifest.mpd" # manifest file
+
+    # opening file
+    file = open_file(mpd_request, connectionSocket)
+    if file == -1:
+        continue 
+    
+    # mpd_file = open(mpd_request, "rb") # open for reading in binary
+    # connectionSocket.send(mpd_file.encode())
+
+    # connectionSocket.send()
+    connectionSocket.close()
